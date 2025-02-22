1fea560974f9356498c3c9e2f0d0f66c40af4828
Jonas <lindinger.jonas.yt@gmail.com>
2025-02-22 15:41:12 +0100

    add: Network and Physics Tick System

---
+++ Assets/_Project/Scripts/CSP/Simulation/NetworkTickSystem.cs
@@ -0,0 +1,10 @@
+﻿namespace _Project.Scripts.CSP.Simulation
+{
+    public class NetworkTickSystem : TickSystem
+    {
+        public override void OnTick(uint tick)
+        {
+            
+        }
+    }
+}
