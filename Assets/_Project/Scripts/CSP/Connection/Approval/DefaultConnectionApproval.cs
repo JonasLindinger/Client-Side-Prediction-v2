6674218f795a514ebc0c91d3ccace5d7ddf808b6
Jonas <lindinger.jonas.yt@gmail.com>
2025-02-22 13:32:21 +0100

    add: Payload decoding when player requests to connect

--- Assets/_Project/Scripts/CSP/Connection/Approval/DefaultConnectionApproval.cs
+++ Assets/_Project/Scripts/CSP/Connection/Approval/DefaultConnectionApproval.cs
@@ -1,4 +1,5 @@
-﻿using Unity.Netcode;
+﻿using _Project.Scripts.CSP.Connection.Data;
+using Unity.Netcode;
 
 namespace _Project.Scripts.CSP.Connection.Approval
 {
@@ -6,7 +7,33 @@ namespace _Project.Scripts.CSP.Connection.Approval
     {
         public override void OnConnectionRequest(NetworkManager.ConnectionApprovalRequest request, NetworkManager.ConnectionApprovalResponse response)
         {
+            ConnectionPayload payload = new ConnectionPayload();
+            payload.LoadFromPayload(request.Payload);
+
+            // Check if Payload is InValid
+            if (!IsValidPayload(payload))
+            {
+                response.Reason = "Invalid payload";
+                response.Approved = false;
+            }
+            
             response.Approved = true;
+        }
+
+        /// <summary>
+        /// Takes in a payload. If the payload is valid, we return true. If it's not, we return false
+        /// </summary>
+        /// <param name="payload"></param>
+        /// <returns></returns>
+        private bool IsValidPayload(ConnectionPayload payload)
+        {
+            if (payload.ClientId == 0)
+                return false;
+            
+            if (string.IsNullOrEmpty(payload.DisplayName.Value))
+                return false;
+
+            return true;
         }
     }
 }
