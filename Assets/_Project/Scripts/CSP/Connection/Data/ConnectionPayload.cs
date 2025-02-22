a3774dea83e6b741e2bd7f4da055d2e2d65b28dc
Jonas <lindinger.jonas.yt@gmail.com>
2025-02-22 13:24:25 +0100

    add: Payload encoding, decoding

--- Assets/_Project/Scripts/CSP/Connection/Data/ConnectionPayload.cs
+++ Assets/_Project/Scripts/CSP/Connection/Data/ConnectionPayload.cs
@@ -1,4 +1,6 @@
-﻿using Unity.Collections;
+﻿using System.Text;
+using Unity.Collections;
+using UnityEngine;
 
 namespace _Project.Scripts.CSP.Connection.Data
 {
@@ -6,5 +8,33 @@ namespace _Project.Scripts.CSP.Connection.Data
     {
         public FixedString32Bytes DisplayName;
         public ulong ClientId;
+
+        #region Encoding
+        
+        private string GetAsJson()
+        {
+            return JsonUtility.ToJson(this);
+        }
+
+        public byte[] GetAsPayload()
+        {
+            return Encoding.UTF8.GetBytes(GetAsJson());
+        }
+        
+        #endregion
+
+        #region Decoding
+        
+        private void LoadFromJson(string json)
+        {
+            this = JsonUtility.FromJson<ConnectionPayload>(json);
+        }
+        
+        public void LoadFromPayload(byte[] payload)
+        {
+            LoadFromJson(Encoding.UTF8.GetString(payload));
+        }
+        
+        #endregion
     }
 }
