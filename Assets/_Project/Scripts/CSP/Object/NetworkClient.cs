cc9e6eced07949a5f4b954bd1e4d7eb262f721bf
Jonas <lindinger.jonas.yt@gmail.com>
2025-02-22 15:28:44 +0100

    add: TickSync.cs, no ticksystem implemented yet

--- Assets/_Project/Scripts/CSP/Object/NetworkClient.cs
+++ Assets/_Project/Scripts/CSP/Object/NetworkClient.cs
@@ -1,7 +1,10 @@
-﻿using Unity.Netcode;
+﻿using _Project.Scripts.CSP.Simulation;
+using Unity.Netcode;
+using UnityEngine;
 
 namespace _Project.Scripts.CSP.Object
 {
+    [RequireComponent(typeof(TickSync))]
     public class NetworkClient : NetworkBehaviour
     {
         
---
+++ Assets/_Project/Scripts/CSP/Simulation/TickSync.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Netcode;
+
+namespace _Project.Scripts.CSP.Simulation
+{
+    public class TickSync : NetworkBehaviour
+    {
+        public override void OnNetworkSpawn()
+        {
+            
+        }
+
+        private void OnTickRPC(uint tick)
+        {
+            
+        }
+    }
+}
