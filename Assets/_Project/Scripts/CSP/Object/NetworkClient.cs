f7497b4c6ecfdd73373835f099d3e693c6951fb4
Jonas <lindinger.jonas.yt@gmail.com>
2025-02-22 14:53:31 +0100

    add: Object spawning

---
+++ Assets/_Project/Scripts/CSP/Object/NetworkClient.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Netcode;
+
+namespace _Project.Scripts.CSP.Object
+{
+    public class NetworkClient : NetworkBehaviour
+    {
+        
+    }
+}
---
+++ Assets/_Project/Scripts/CSP/Object/Spawner.cs
@@ -0,0 +1,56 @@
+﻿using _Project.Scripts.Utility;
+using UnityEngine;
+
+namespace _Project.Scripts.CSP.Object
+{
+    public class Spawner : MonoBehaviourSingleton<Spawner>
+    {
+        [Header("References")]
+        [SerializeField] private NetworkClient networkClientPrefab;
+        [SerializeField] private NetworkPlayer networkPlayerPrefab;
+
+        #if Server
+        public void SpawnClient(ulong clientId)
+        {
+            var client = Instantiate(networkClientPrefab);
+            client.NetworkObject.AlwaysReplicateAsRoot = false;
+            client.NetworkObject.SynchronizeTransform = false;
+            client.NetworkObject.ActiveSceneSynchronization = false;
+            client.NetworkObject.SceneMigrationSynchronization = false;
+            client.NetworkObject.AutoObjectParentSync = false;
+            client.NetworkObject.SyncOwnerTransformWhenParented = false;
+            client.NetworkObject.AllowOwnerToParent = false;
+            
+            // Only the client and we as the server should see the object
+            client.NetworkObject.SpawnWithObservers = false;
+            
+            // This object should be destroyed when the owner leaves
+            client.NetworkObject.DontDestroyWithOwner = false;
+            
+            client.NetworkObject.SpawnWithOwnership(clientId);
+            client.NetworkObject.NetworkShow(clientId);
+        }
+        
+        public void SpawnPlayer(ulong clientId)
+        {
+            var player = Instantiate(networkPlayerPrefab);
+            player.NetworkObject.AlwaysReplicateAsRoot = false;
+            player.NetworkObject.SynchronizeTransform = false;
+            player.NetworkObject.ActiveSceneSynchronization = false;
+            player.NetworkObject.SceneMigrationSynchronization = false;
+            player.NetworkObject.AutoObjectParentSync = false;
+            player.NetworkObject.SyncOwnerTransformWhenParented = false;
+            player.NetworkObject.AllowOwnerToParent = false;
+            
+            // This object should be seen by everyone
+            player.NetworkObject.SpawnWithObservers = true;
+            
+            // This object should NOT be destroyed when the owner leaves
+            player.NetworkObject.DontDestroyWithOwner = true;
+            
+            player.NetworkObject.SpawnWithOwnership(clientId);
+        }
+        
+        #endif
+    }
+}
