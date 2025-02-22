591d7ad86ff93a4e02962ad87c9b7694bfd455ec
Jonas <lindinger.jonas.yt@gmail.com>
2025-02-22 11:10:56 +0100

    add: Load into MainMenu

--- Assets/_Project/Scripts/CSP/NetworkRunner.cs
+++ Assets/_Project/Scripts/CSP/NetworkRunner.cs
@@ -1,7 +1,26 @@
-﻿namespace _Project.Scripts.CSP
+﻿using System.Threading.Tasks;
+using _Project.Scripts.Utility;
+using _Project.Scripts.Utility.SceneManagement;
+
+namespace _Project.Scripts.CSP
 {
-    public class NetworkRunner
+    public class NetworkRunner : MonoBehaviourSingleton<NetworkRunner>
     {
+        private async void Start()
+        {
+            await LoadIntoMainMenu();
+        }
         
-    }   
+        #region SceneLoader
+
+        /// <summary>
+        /// Loads the scene Group of MainMenu
+        /// </summary>
+        private async Task LoadIntoMainMenu()
+        {
+            await SceneLoader.GetInstance().LoadSceneGroup(0);
+        }
+        
+        #endregion
+    }
 }
